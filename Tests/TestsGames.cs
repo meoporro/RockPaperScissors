5eb47c30d1aed6644b018d80f04613fe4232948b
Matteo Porro <meoo85@gmail.com>
2018-01-06 18:26:08 +0000

    Create separate test classes.

--- Tests/TestsGames.cs
+++ Tests/TestsGames.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Games;
-using Players;
+
 
 namespace Tests
 {
     [TestClass]
-    public class Tests
+    public class TestsGames
     {
         [TestMethod]
         public void TestGameFactoryRockPaperScissorsGame()
@@ -21,38 +21,12 @@ namespace Tests
         }
 
         // Add a test that catched the exception
-
-        [TestMethod]
-        public void TestPlayerFactoryHumanPlayer()
-        {
-            WorkerTestPlayerFactory(SupportedPlayers.HumanPlayer, typeof(HumanPlayer));
-        }
-
-        [TestMethod]
-        public void TestPlayerFactoryComputerPlayer()
-        {
-            WorkerTestPlayerFactory(SupportedPlayers.ComputerPlayer, typeof(ComputerPlayer));
-        }
-
-        [TestMethod]
-        public void TestPlayerFactoryStrategicComputerPlayer()
-        {
-            WorkerTestPlayerFactory(SupportedPlayers.StrategicComputerPlayer, typeof(StrategicComputerPlayer));
-        }
-
+        
         private void WorkerTestGameFactory(SupportedGames selectedGameType, Type expectedGameType)
         {
             var NumberOfTurns = 3;
             var CreatedGame = Game.CreateGame(selectedGameType, NumberOfTurns);
             Assert.IsInstanceOfType(CreatedGame, expectedGameType);
         }
-
-        private void WorkerTestPlayerFactory(SupportedPlayers selectedPlayerType, Type playerType)
-        {
-            var CreatedPlayer = Player.CreatePlayer(selectedPlayerType);
-            Assert.IsInstanceOfType(CreatedPlayer, playerType);
-        }
-
-
     }
 }
---
+++ Tests/TestsPlayers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Players;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestsPlayers
+    {
+        [TestMethod]
+        public void TestPlayerFactoryHumanPlayer()
+        {
+            WorkerTestPlayerFactory(SupportedPlayers.HumanPlayer, typeof(HumanPlayer));
+        }
+
+        [TestMethod]
+        public void TestPlayerFactoryComputerPlayer()
+        {
+            WorkerTestPlayerFactory(SupportedPlayers.ComputerPlayer, typeof(ComputerPlayer));
+        }
+
+        [TestMethod]
+        public void TestPlayerFactoryStrategicComputerPlayer()
+        {
+            WorkerTestPlayerFactory(SupportedPlayers.StrategicComputerPlayer, typeof(StrategicComputerPlayer));
+        }
+
+        private void WorkerTestPlayerFactory(SupportedPlayers selectedPlayerType, Type playerType)
+        {
+            var CreatedPlayer = Player.CreatePlayer(selectedPlayerType);
+            Assert.IsInstanceOfType(CreatedPlayer, playerType);
+        }
+    }
+}
